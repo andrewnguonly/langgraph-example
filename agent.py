fcd304b6ce21b03649e78ac84135777f773e801d
Andrew Nguonly <andrewnguonly@gmail.com>
2024-12-26 10:26:32 -0800

    Update agent state.

--- agent.py
+++ agent.py
@@ -25,6 +25,8 @@ def _get_model(model_name: str):
 
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
+    num_msgs: int
+    is_even: bool
 
 
 # Define the function that determines whether to continue or not
@@ -46,8 +48,10 @@ def call_model(state, config):
     model = _get_model(model_name)
     response = model.invoke(messages)
 
+    num_msgs = len(messages) + 1
+
     # We return a list, because this will get added to the existing list
-    return {"messages": [response]}
+    return {"messages": [response], "num_msgs": num_msgs, "is_even": num_msgs % 2 == 0}
 
 
 # Define the function to execute tools
