8abb3bcc74f6d7d663fdd30f6bc66e5d72370914
Andrew Nguonly <andrewnguonly@gmail.com>
2025-03-27 16:17:26 -0700

    Update node call to stream JSON.

--- my_agent/utils/nodes.py
+++ my_agent/utils/nodes.py
@@ -3,6 +3,7 @@ from langchain_anthropic import ChatAnthropic
 from langchain_openai import ChatOpenAI
 from my_agent.utils.tools import tools
 from langgraph.prebuilt import ToolNode
+from langchain_core.output_parsers import JsonOutputParser
 
 
 @lru_cache(maxsize=4)
@@ -37,7 +38,11 @@ def call_model(state, config):
     messages = [{"role": "system", "content": system_prompt}] + messages
     model_name = config.get('configurable', {}).get("model_name", "anthropic")
     model = _get_model(model_name)
-    response = model.invoke(messages)
+
+    # stream JSON output
+    chain = (model | JsonOutputParser())
+    response = chain.invoke(messages)
+
     # We return a list, because this will get added to the existing list
     return {"messages": [response]}
 
